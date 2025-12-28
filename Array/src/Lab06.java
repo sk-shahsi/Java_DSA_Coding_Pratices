80a832cc381fa9d3db2826dfc5d16e18a8c0bf2a
sk-shahsi <sk.sashikumar055@gmail.com>
2025-12-28 13:14:30 +0530

    update

--- Array/src/Lab06.java
+++ Array/src/Lab06.java
@@ -1,2 +1,15 @@
 public class Lab06 {
+    public static void main(String[] args) {
+        int arr [] ={1,2,45,86,79,2,9};
+        int count=0;
+        for (int number:arr){
+            if(number % 2 ==0){
+                count ++;
+                System.out.println("EvenNumber "+number+ " count "+count);
+            }
+            else {
+                System.out.println("Odd Number"+number+" count"+ count);
+            }
+        }
+    }
 }
